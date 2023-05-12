6a10e3c46e7c741c214570afd7fa6cd79cbced24
diego1193 <diegocabrera1193@gmail.com>
2023-05-12 00:05:01 -0500

    Algunos manejos basicos de DOM

--- main/main_evento.js
+++ main/main_evento.js
@@ -3,6 +3,10 @@ const input2 = document.querySelector('#calculo2');
 const pResult = document.querySelector('#resultado')
 const btn = document.querySelector('#btnCalcular');
 const form = document.querySelector('#form')
+const div_my = document.querySelector('.class-div')
+const window_my = document.querySelector("iframe")
+const div_sec = document.querySelector("#div_sec")
+const p_iframe = document.querySelector("#p_iframe")
 // elemento.addEventListener('<evento al q queramos llamar>', <funcion_sun parentesis>)
 // .addEventListener -> Lo q hace es escuchar cualquier evento q pase en ese elemento
 // <evento al q queramos llamar> -> Espeficificamos de todos los eventos cual esd el q queremos llamar
@@ -31,8 +35,39 @@ const form = document.querySelector('#form')
 //////////////// EVENT segunda FORMA para trabajarlo como un boton////////////////
 // SI EN EL HTML <button type="button" id="btnCalcular">Calcular</button>
 
+
+div_my.addEventListener('mouseover', mauseover); //cuando pasamos el mause encima del div
+function mauseover (){
+    console.log("Entra");
+    div_my.style.backgroundColor = 'yellow';
+}
+
+div_my.addEventListener('mouseout', () => { // Cuando quitamos el mouse encima del div
+    div_my.style.backgroundColor = 'white';
+} )
+
 btn.addEventListener('click', clickButtom);// Evento con formulario con type="buttton"
 function clickButtom(){
     const sumaInput = parseInt(input1.value) + parseInt(input2.value);
     pResult.innerText = 'Resultado: ' + sumaInput;
 }
+
+
+window_my.addEventListener('load', () => { // Evnto cuando carga correctamente un iframe, ancla, div, imagen
+    console.log("la pagina se a cargado")
+})
+
+/* Events fired on the drag target */
+document.addEventListener("dragstart", function(event) {
+    event.dataTransfer.setData("Text", event.target.id);// Se especifica q tipo de dato se va a mover o trasportar
+    document.getElementById("demo").innerHTML = "Started dragging.";
+  });
+  
+  document.addEventListener("dragend", function(event) {
+    document.getElementById("demo").innerHTML = "Finished dragging.";
+  });
+  
+  /* Events fired on the drop target */
+  document.addEventListener("dragover", function(event) {
+    event.preventDefault();
+  });
